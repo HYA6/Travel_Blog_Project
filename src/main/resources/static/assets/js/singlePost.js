c39e85d5be93ac3134ec8d4803ebd55502639ada
LEEHAYOUNG <sjo6382@naver.com>
2023-10-25 18:06:01 +0900

    231025 update

--- src/main/resources/static/assets/js/singlePost.js
+++ src/main/resources/static/assets/js/singlePost.js
@@ -1,4 +1,15 @@
 // 댓글 답글 입력창 표시
-function replyOpen(commentId) {
-	$('#reply' + commentId).show();
+function replyOpen(event) {
+	let eventId = event.getAttribute('id');
+	let commentId = eventId.subString(8);
+	console.log(commentId);
+//	$('#reply' + commentId).show();
+};
+
+// 답글 더보기
+function replyOpenMore(event) {
+	let eventId = event.getAttribute('id');
+	let commentGup = eventId.substring(9);
+	console.log(commentGup);
+//	$('#replyOpenMore' + commentGup).show();
 };
